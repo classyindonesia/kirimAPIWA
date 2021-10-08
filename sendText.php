26245181cf7f58398d1640c946078f020929e6a3
classyindonesia <79906833+classyindonesia@users.noreply.github.com>
2021-10-08 22:53:24 +0700

    Update sendText.php
    Penambahan Server API

--- sendText.php
+++ sendText.php
@@ -6,7 +6,7 @@ $params = [
     "to"=>"6285648811419",
     "pesan"=>"kode otp anda '3212'. jangan beritahukan ke siapapun untuk menjaga keamanan akun anda"
 ];
-$url = "http://192.168.225.132:5000/waapi/sendText";
+$url = "http://103.41.204.234:50010/waapi/sendText";
 
 $d = post_without_wait($url,$params);
 print_r($d);
